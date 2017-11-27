117275bfa308f59867ac43ebc51d3c0e542e8049
Joseph-Marlitt <joseph.marlitt@gmail.com>
2017-11-27 16:56:37 -0600

    draft

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-	
+	 var questionTimer;
+//Start button starts the timer and reveals the questions
+	$("#start").on("click", function() {
+        //  Set the button alert's timeout to run three seconds after the function's called.
+        questionTimer = setTimeout(function() {
+          console.log("Times Up!");
+        }, 30000);
+
+
+      });
+// 
 }
