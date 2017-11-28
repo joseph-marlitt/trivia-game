b8e24a7ef763f6c5689196b1de6ed29bb1a35bf3
Joseph-Marlitt <joseph.marlitt@gmail.com>
2017-11-28 11:32:21 -0600

    draft

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
 	
-
+	$("#quiz").hide();
 	 var questionTimer;
 //Start button starts the timer and reveals the questions
 	$("#start").on("click", function() {
+
+		$("#quiz").show();
         //  Set the question to run thirty seconds after the function's called.
         questionTimer = setTimeout(function() {
           console.log("Times Up!");
         }, 30000);
 
-    
 
       });
 // 
