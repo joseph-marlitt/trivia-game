10e68fcb65cfe5962cd11b2a15322901afe38ad9
Joseph-Marlitt <joseph.marlitt@gmail.com>
2017-11-29 16:47:26 -0600

    draft

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,23 @@
 $(document).ready(function() {
-	
 
-var quizResults;
+//List of Variables for the game	
 var questionTimer;
 $("#quiz").hide();
 	 
 //Start button starts the timer and reveals the questions
 	$("#start").on("click", function() {
-		//variable representing quiz results.
-		quizResults = function (){
-				$( "input[type=radio][name=answer][value=right]:checked" ).val();
-			}
-
+		//show quiz
 		$("#quiz").show();
+		//variable representing quiz results.
+		var quizResults = $('[type=radio][name=answers]:checked').val();  
         //  Set the question to run thirty seconds after the function's called.
-    questionTimer = setTimeout(function() {
-          console.log("Times Up!");
-          console.log(quizResults);
-          $("#results").html("You scored "	+ quizResults + "answers correct!")
-          $("#quiz").hide();
+		questionTimer = setTimeout(function() {
+	      console.log("Times Up!");
+	      
+	      $("#quiz").hide();
+	      	
+	      	console.log(quizResults);
+	      	$("#results").html("You scored " + quizResults + " answers correct!")
         }, 5000);
-      });
-// 
-	if (questionTimer == 0){
-		
-
-	}
-// 
-});
+	})
+})
